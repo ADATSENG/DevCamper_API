fa325bf8e5076a9bdd34a26522be1f8ab3775390
baifu-ada <ada.zeng@baifu-tech.net>
2020-12-31 21:42:07 +0800

    update advanced Results Middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db')
 
@@ -25,9 +27,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// File uploading
+app.use(fileupload())
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
